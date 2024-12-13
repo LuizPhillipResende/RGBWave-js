c9543cf6f11b86268c01bb34ed722fead37e3257
LuizPhillipResende <62450475+LuizPhillipResende@users.noreply.github.com>
2024-12-13 15:47:12 -0300

    Delete Desktop directory

--- Desktop/gerencia-comp/Grao_Uai-main/scripts/general.js
+++
@@ -1,6 +0,0 @@
-// garantir que página volte ao início após recarregamento
-window.onbeforeunload = function () {
-    if ('scrollRestoration' in history) {
-        history.scrollRestoration = 'manual';
-    }
-}
